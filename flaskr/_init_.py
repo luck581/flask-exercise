08871a4c63034b5dc3b4e761be84c37e9a73ecab
luck581 <1712535818@qq.com>
2021-06-23 09:41:36 +0000

    ADD

--- flaskr/_init_.py
+++ flaskr/_init_.py
@@ -29,12 +29,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-  from . import db
+    from . import db
     db.init_app(app)
 
-    
     return app
-
-
-
-  
